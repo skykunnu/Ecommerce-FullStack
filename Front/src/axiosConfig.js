791ee70f1322865864b5f25719f06ac1ab899c15
Shikhar Khandelwal <skykunnu@gmail.com>
2025-02-19 13:04:58 +0530

    Front

--- Front/src/axiosConfig.js
+++ Front/src/axiosConfig.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-const instance=axios.create({baseURL:"https://ecommerce-api-8ga2.onrender.com/api"});
+const instance = axios.create({
+//   baseURL: "https://ecommerce-api-8ga2.onrender.com/api",
+baseURL:"http://localhost:4000/api"
+});
 
 export default instance;
-
 
 // For more Knowledge
 
